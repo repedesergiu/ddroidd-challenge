45b28416e6305b411656b1fde075b554d6372bd3
DESKTOP-QSF86GA\sergi <sergiurepedeionut@gmail.com>
2021-10-31 22:14:09 +0200

    remove typo

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -80,7 +80,7 @@ export default function Cart(props) {
           className={classes.white}
           fontWeight="600"
         >
-          dadada Cart
+          Cart
           {props.itemsNumber ? ` ( ${props.itemsNumber} ) ` : null}
         </Typography>
       </div>
